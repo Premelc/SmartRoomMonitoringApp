7716cef7429be7a799ae5a7d31999fe3e3a82972
Dominik Premelč <dominik.premelc33@gmail.com>
2022-05-15 18:48:55 +0200

    Initial commit

---
+++ src/test/java/com/premelc/smartroommonitoringapp/SmartRoomMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premelc.smartroommonitoringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRoomMonitoringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
