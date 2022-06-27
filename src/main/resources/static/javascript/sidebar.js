f6bff08064db8d1c4cef69080201f7678e4b72d9
Dominik Premelč <dominik.premelc33@gmail.com>
2022-06-27 04:17:15 +0200

    added Highchart + several graphing capabilities +  graph boosting for big data

--- src/main/resources/static/javascript/sidebar.js
+++ src/main/resources/static/javascript/sidebar.js
@@ -1,5 +1,4 @@
 let flag = false;
-
 function toggleNav(){
     if(flag === true){
         console.log(flag)
@@ -9,21 +8,16 @@ function toggleNav(){
         openNav();
     }
 };
-
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     fadeOut( document.getElementById("right") , document.getElementById("middle"));
     flag = true;
-
 }
-
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     fadeIn( document.getElementById("right"), document.getElementById("middle"));
     flag = false;
-
 }
-
 function fadeOut(element1, element2) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
@@ -37,7 +31,6 @@ function fadeOut(element1, element2) {
         op -= op * 0.1;
     }, 15);
 }
-
 function fadeIn(element1, element2) {
     var op = 0.1;  // initial opacity
     var timer = setInterval(function () {
@@ -51,44 +44,54 @@ function fadeIn(element1, element2) {
         op += op * 0.1;
     }, 15);
 }
-
 function openRoomStatusForm(){
     closeNav();
     closeRoomArchiveStatusForm();
     closeRoomOnDayForm();
+    closeChartPopup()
     document.getElementById("roomStatusForm").style.display = "block";
-
 }
-
-function closeRoomStatusForm(){
-    document.getElementById("roomStatusForm").style.display = "none";
-}
-
 function openRoomArchiveStatusForm(){
     closeNav();
     closeRoomStatusForm();
     closeRoomOnDayForm();
+    closeChartPopup()
     document.getElementById("roomArchiveStatusForm").style.display = "block";
 }
-function closeRoomArchiveStatusForm(){
-    document.getElementById("roomArchiveStatusForm").style.display = "none";
-}
-
 function openRoomOnDayForm(){
     closeNav();
     closeRoomStatusForm();
     closeRoomArchiveStatusForm();
+    closeChartPopup()
     document.getElementById("roomStatusOnDay").style.display = "block";
 }
-
-function closeRoomOnDayForm(){
-    document.getElementById("roomStatusOnDay").style.display = "none";
+function openChartPopup(){
+    closeNav();
+    closeRoomStatusForm();
+    closeRoomArchiveStatusForm();
+    closeRoomOnDayForm();
+    document.getElementById("chartPopup").style.display = "block";
 }
 function openPopupRoomState(){
     closeNav();
     document.getElementById("popupRoomStateContainer").style.display = "block";
 }
-
+function closeRoomOnDayForm(){
+    document.getElementById("roomStatusOnDay").style.display = "none";
+}
 function closePopupRoomState(){
     document.getElementById("popupRoomStateContainer").style.display = "none";
 }
+function closeChartPopup(){
+    document.getElementById("chartPopup").style.display = "none";
+}
+function closeRoomArchiveStatusForm(){
+    document.getElementById("roomArchiveStatusForm").style.display = "none";
+}
+function closeRoomStatusForm(){
+    document.getElementById("roomStatusForm").style.display = "none";
+}
+
+
+
+
